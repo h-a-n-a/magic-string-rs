e771362d6bed288a3a32a666725e679ac5294c36
wangshuyuan.h <wangshuyuan.h@bytedance.com>
2021-12-09 22:14:44 +0800

    perf(node): add binding memoization

--- node/src/utils.ts
+++ node/src/utils.ts
@@ -1,11 +1,17 @@
 import { resolve } from 'path'
 import { loadBinding } from '@node-rs/helper'
 
-export const getMagicString = (): any => {
-  const { MagicString } = loadBinding(
-    resolve(__dirname, '../'),
-    'magic-string-rs',
-    '@napi-rs/magic-string'
-  )
-  return MagicString
-}
+export const getMagicString = (() => {
+  let binding = null
+  return (): any => {
+    if (binding) return binding
+
+    const { MagicString } = loadBinding(
+      resolve(__dirname, '../'),
+      'magic-string-rs',
+      '@napi-rs/magic-string'
+    )
+
+    return (binding = MagicString)
+  }
+})()
